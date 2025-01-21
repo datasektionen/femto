e900b76a7fd92da9c6d2db05d619ce7f03e8f56e
Arman Montazeri <armanmo@kth.se>
2025-01-21 12:32:27 +0100

    add Login and Home views with routing setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,47 @@
 import React from "react";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Methone from "methone";
 import { MantineProvider } from "@mantine/core";
+import Home from './views/Home.tsx';
+import Login from './views/Login.tsx';
 
 const App = () => {
-    const config = {
-        system_name: "link-shortener",
-        login_href: "/login",
-        login_text: "Logga in",
-        color_scheme: "light-blue",
-        links: [
-            <Link to="/shorten" key="methone-link-1">
-                Förkorta
-            </Link>,
-        ],
-    };
+  // taken example from methone documentation
+  const config = {
+    system_name: "link-shortener",
+    login_href: "/login",
+    login_text: "Logga in",
+    color_scheme: "light-blue",
+    links: [
+      <Link to="/shorten" key="methone-link-1">
+        Förkorta
+      </Link>,
+    ],
+  };
 
-    return (
-        <MantineProvider
-            theme={{
-                fontFamily: "Lato",
-                headings: { fontFamily: "Lato" },
-                primaryColor: "blue",
-            }}
-        >
-            <BrowserRouter>
-                <div id="application" className="light-blue">
-                    <Methone config={config} />
-                    <div style={{ padding: "20px", textAlign: "center" }}>
-                        <h1>Welcome to the Example App</h1>
-                        <p>This is an example body text on a blank page.</p>
-                    </div>
-                </div>
-            </BrowserRouter>
-        </MantineProvider>
-    );
+  return (
+    // MantineProvider is a wrapper for the entire application
+    <MantineProvider
+      theme={{
+        fontFamily: "Lato",
+        headings: { fontFamily: "Lato" },
+        primaryColor: "blue",
+      }}
+    >
+      <BrowserRouter>
+        <div id="application" className="light-blue" style={{ marginTop: "50px" }}>
+          {/* Methone component is used to render the header */}
+          <Methone config={config} />
+          <Routes>
+            {/* Define routes for Home and Login */}
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/shorten" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </MantineProvider>
+  );
 };
 
 export default App;
