99e250f7ece6c8623eccac8a016952d45ce22df8
Maximilian Benedicto <mbene@kth.se>
2025-02-28 08:49:57 +0100

    Add blacklist controller with functions to manage links

---
+++ server/src/controllers/blacklistController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+
+/**
+ * Adds a link to the blacklist.
+ * 
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ * @returns {Promise<void>} - A promise that resolves when the link is added to the blacklist.
+ */
+export async function addLinkBlacklist(req: Request, res: Response): Promise<void> {
+    // TODO: Implement function
+};
+
+/**
+ * Removes a link from the blacklist.
+ * 
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ * @returns {Promise<void>} - A promise that resolves when the link is removed from the blacklist.
+ */
+export async function removeLinkBlacklist(req: Request, res: Response): Promise<void> {
+    // TODO: Implement function
+};
+
+/**
+ * Retrieves all links from the blacklist.
+ * 
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ * @returns {Promise<void>} - A promise that resolves when the links are retrieved from the blacklist.
+ */
+export async function getBlacklist(req: Request, res: Response): Promise<void> {
+    // TODO: Implement function
+};
