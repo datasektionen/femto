71647fa4a694f392a5a956776b0f4d83cdd924fa
Arman Montazeri <armanmo@kth.se>
2025-04-23 09:21:12 +0200

    Update .gitignore to include additional files and directories

--- client/src/configuration.ts
+++ client/src/configuration.ts
@@ -3,16 +3,12 @@ const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET || "";
 
 interface AppConfig {
   loginApiUrl: string;
-  oidcIssuer: string;
   clientId: string;
-  clientSecret: string;
 }
 
 const Configuration: AppConfig = {
   loginApiUrl: "https://sso.datasektionen.se",
-  oidcIssuer: "https://sso.datasektionen.se/op",
   clientId: CLIENT_ID,  
-  clientSecret: CLIENT_SECRET, 
 };
 
 export default Configuration;
