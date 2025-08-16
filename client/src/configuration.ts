edd8f8ce50ff846df672fca1eb1ef7cf0b789ad8
Viktor Ekby <viktoe@datasektionen.se>
2025-08-16 23:13:21 +0200

    fix: default client url

--- client/src/configuration.ts
+++ client/src/configuration.ts
@@ -1,4 +1,4 @@
-const backendApiUrl = import.meta.env.VITE_BACKEND_ROOT || "https://femto.betasektionen.se";
+const backendApiUrl = import.meta.env.VITE_BACKEND_ROOT || "https://femto.datasektionen.se";
 
 /**
  * This file contains the configuration for the application.
